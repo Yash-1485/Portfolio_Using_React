51b392853a0636052509cce09fdcc71f2a1b2ebe
Yash1485 <yashparekh140805@gmail.com>
2025-06-05 23:39:04 +0530

    Added Landing Page Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import LoadingScreen from "./components/LoadingScreen";
+
 function App() {
+
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <>
-      
+      {!isLoaded && <LoadingScreen onComplete={() => { setIsLoaded(true) }} />}
+      <div className={`min-h-screen bg-black text-gray-100 transition-opacity duration-500 ${isLoaded?"opacity-100":"opacity-0"}`}>
+    
+      </div>
     </>
   )
 }
